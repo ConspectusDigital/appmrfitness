6ad775084f20d5fe5523cac0f1af4b3bbe7132ef
ConspectusDigital <conspectusdigital@gmail.com>
2025-06-23 15:04:40 -0300

    Create CalculadoraFinanceira.jsx

---
+++ src/components/CalculadoraFinanceira.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CalculadoraFinanceira() {
+  return (
+    <div className="bg-white rounded-2xl shadow p-6 max-w-xl mx-auto my-8">
+      <h2 className="font-bold text-xl mb-2 text-black">Calculadora Financeira</h2>
+      <div className="text-black">[Simulação financeira e gráficos aqui em breve!]</div>
+    </div>
+  );
+}
