0f4240976ceb8c54cc9b8bfda79618722c3f0609
ConspectusDigital <conspectusdigital@gmail.com>
2025-06-23 15:40:55 -0300

    Create index.js

---
+++ src/components/etapas/index.js
@@ -0,0 +1,6 @@
+export { default as EtapaLegalizacao } from "./EtapaLegalizacao";
+export { default as EtapaPlanejamento } from "./EtapaPlanejamento";
+export { default as EtapaEstrutura } from "./EtapaEstrutura";
+export { default as EtapaGestao } from "./EtapaGestao";
+export { default as EtapaMarketing } from "./EtapaMarketing";
+export { default as EtapaChecklistFinal } from "./EtapaChecklistFinal";
