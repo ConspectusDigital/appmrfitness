71e1f235b61a4cd4f65c79278e73ca104ca4fe44
ConspectusDigital <conspectusdigital@gmail.com>
2025-06-23 15:05:34 -0300

    Create Conquistas.jsx

---
+++ src/components/Conquistas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Conquistas() {
+  return (
+    <div className="bg-white rounded-2xl shadow p-6 max-w-xl mx-auto my-8">
+      <h2 className="font-bold text-xl mb-2 text-black">Minhas Conquistas</h2>
+      <div className="text-black">[Lista de medalhas, streaks, XP total, certificados futuramente!]</div>
+    </div>
+  );
+}
