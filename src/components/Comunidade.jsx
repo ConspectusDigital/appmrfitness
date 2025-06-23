feb5df5aa33c2ebf212e36d2ef5752d3101b3bb1
ConspectusDigital <conspectusdigital@gmail.com>
2025-06-23 15:05:10 -0300

    Create Comunidade.jsx

---
+++ src/components/Comunidade.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Comunidade() {
+  return (
+    <div className="bg-white rounded-2xl shadow p-6 max-w-xl mx-auto my-8">
+      <h2 className="font-bold text-xl mb-2 text-black">Comunidade & FAQ</h2>
+      <div className="text-black">[Fórum, perguntas, integrações aqui futuramente!]</div>
+    </div>
+  );
+}
